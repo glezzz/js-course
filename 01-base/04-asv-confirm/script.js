2f682701bbbe092dbdaa666a1fc43c055d776744
glezzz <alexglezk@gmail.com>
2020-08-25 11:18:18 +0200

    finished-exercise

--- 01-base/04-asv-confirm/script.js
+++ 01-base/04-asv-confirm/script.js
@@ -11,6 +11,16 @@
 
 (function() {
 
-    // your code here
+    var age = window.prompt("How old are you?");
+    var gender = window.prompt("Are you male or female or offended?");
+    var town = window.prompt("Where do you live?");
+
+    if (confirm(age + " " + gender + " " + town)) {
+
+    }else {
+        var age = window.prompt("How old are you?");
+        var gender = window.prompt("Are you male or female or offended?");
+        var town = window.prompt("Where do you live?");
+    }
 
 })();
