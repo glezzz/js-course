3b415107877aa284fc5e9e06a8f2d3bbfee34c46
glezzz <alexglezk@gmail.com>
2020-08-30 20:35:24 +0200

    redid 2.2 shorter method

--- 02-maths/02-calculator-two/script.js
+++ 02-maths/02-calculator-two/script.js
@@ -10,7 +10,38 @@
 // You will have time to focus on it later.
 
 (function() {
-    let one;
+    // to get the value of an input: document.getElementById("element-id").value
+
+    var performOperation = function(operation) {
+        // perform the operation
+        var a = parseInt(document.getElementById("op-one").value);
+        var b = parseInt(document.getElementById("op-two").value);
+
+        switch (operation) {
+            case "addition":
+                alert(a + b);
+                break;
+            case "substraction":
+                alert(a - b);
+                break;
+            case "multiplication":
+                alert(a * b);
+                break;
+            case "division":
+                alert(a / b);
+                break;
+        }
+    };
+
+    Array.from(document.querySelectorAll("button.operator")).forEach(function($btn) {
+        $btn.addEventListener("click", function() {
+            performOperation($btn.id);
+        });
+    });
+})();
+
+/* first attempt
+let one;
     let two;
 
     var performOperation = function(operation) {
@@ -46,5 +77,4 @@
     function checkInput(){
         one = document.getElementById("op-one").value;
         two = document.getElementById("op-two").value;
-    }
-})();
+ */
