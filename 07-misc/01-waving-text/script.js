eafba78e548b15c65354bf7e43b36eed23229fd1
glezzz <alexglezk@gmail.com>
2020-09-02 09:07:55 +0200

    added folder chapter 7

---
+++ 07-misc/01-waving-text/script.js
@@ -0,0 +1,17 @@
+/* becode/javascript
+ *
+ * /07-misc/01-waving-text/script.js - 7.1: effet vague
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    // your code here
+    document.getElementById("target").style.fontSize = "xx-small"
+
+})();
