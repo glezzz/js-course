49d819fcb1eb11c6c8e5c6edfc753f070f75fec2
glezzz <alexglezk@gmail.com>
2020-09-09 14:36:03 +0200

    finished 9.2 whelp

--- 09-fetch/02-list-to-template/script.js
+++ 09-fetch/02-list-to-template/script.js
@@ -12,9 +12,22 @@
 (() => {
     // your code here
     let run = document.getElementById("run")
-    run.addEventListener("click", function (){
+    run.addEventListener("click", function () {
         fetch("http://localhost:3000/heroes")
             .then(response => response.json())
-            .then();
+            .then(data => appendData(data))
+
+        function appendData(data) {
+            let target = document.getElementById("target");
+            for (let i = 0; i < data.length; i++) {
+                let tplate = document.getElementById("tpl-hero").content.cloneNode(true);//this is to get the X-Men list from HTML file
+                tplate.querySelector('.name').innerHTML = data[i].name;
+                tplate.querySelector('.alter-ego').innerHTML = data[i].alterEgo;
+                tplate.querySelector('.powers').innerHTML = data[i].abilities;
+                target.appendChild(tplate);
+                console.log(data);
+
+            }
+        }
     })
 })();
