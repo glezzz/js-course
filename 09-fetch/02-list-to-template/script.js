c961123f7f5d7db89c456ec0c9d9de2a7abae2da
glezzz <alexglezk@gmail.com>
2020-09-09 15:37:44 +0200

    9.2 added some comments

--- 09-fetch/02-list-to-template/script.js
+++ 09-fetch/02-list-to-template/script.js
@@ -15,14 +15,14 @@
     run.addEventListener("click", function () {
         fetch("http://localhost:3000/heroes")
             .then(response => response.json())
-            .then(data => appendData(data))
+            .then(data => appendData(data)) // here we need to attach data not console log
 
-        function appendData(data) {
+        function appendData(data) { // function to style the data
             let target = document.getElementById("target");
             for (let i = 0; i < data.length; i++) {
                 let tplate = document.getElementById("tpl-hero").content.cloneNode(true);//this is to get the X-Men list from HTML file
                 tplate.querySelector('.name').innerHTML = data[i].name;
-                tplate.querySelector('.alter-ego').innerHTML = data[i].alterEgo;
+                tplate.querySelector('.alter-ego').innerHTML = data[i].alterEgo; // names from api.json
                 tplate.querySelector('.powers').innerHTML = data[i].abilities;
                 target.appendChild(tplate);
                 console.log(data);
