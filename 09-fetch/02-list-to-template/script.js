b284a5a88a4fa4275f1bcff99b478ede8cb5544b
glezzz <alexglezk@gmail.com>
2020-09-09 12:31:16 +0200

    prelunch commit working on 9.2

--- 09-fetch/02-list-to-template/script.js
+++ 09-fetch/02-list-to-template/script.js
@@ -11,4 +11,10 @@
 
 (() => {
     // your code here
+    let run = document.getElementById("run")
+    run.addEventListener("click", function (){
+        fetch("http://localhost:3000/heroes")
+            .then(response => response.json())
+            .then();
+    })
 })();
